fcf921d70fc48a202739042a43e059fd11b5f402
hungdmph36281 <hungdmph36281@fpt.edu.vn>
2024-10-07 22:16:50 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::post('/addd1', [AuthController::class, 'addd2']);
+Route::post('/addd3', [AuthController::class, 'addd3']);
 
