2a7de52e9b25e5b5349140e00a064be661915e67
truuongtd128 <tranductruong2k4hb@gmai.com>
2024-10-14 02:01:20 +0700

    kk

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,6 @@ Route::prefix('/address')->group(function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-// Route::get('/user', [AuthController::class, 'user']);
+
 
 Route::middleware('auth:sanctum')->get('user', [AuthController::class, 'getUserInfo']);
