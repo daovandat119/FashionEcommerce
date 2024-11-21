d19df4932da668cfe66241cada7cd89f8e07b32c
truuongtd128 <tranductruong2k4hb@gmail.com>
2024-11-21 19:06:35 +0700

    f

--- resources/views/emails/order-placed.blade.php
+++ resources/views/emails/order-placed.blade.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <h1>Thank you for your order, {{ $order['UserName'] }}</h1>
+    <p>Your Order Code: <strong>{{ $order['OrderCode'] }}</strong></p>
     <p>We will notify you once your order is shipped.</p>
     <p>Total Amount: <strong>{{ $order['TotalAmount'] }} VND</strong></p>
     <p>Thank you for shopping with us!</p>
